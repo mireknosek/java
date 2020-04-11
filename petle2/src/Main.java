0edc52cd11e8839391915eb26c6e63397952a728
mireknosek <mireknosek@gmail.com>
2020-04-11 15:04:27 +0200

    petle2

---
+++ petle2/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 10;
+        while (i < 0) {
+            System.out.println(i);
+            i--;
+        }
+        System.out.println();
+
+        //pętla do-while wykona sie jeden raz
+        i = 10;
+        do {
+            System.out.println(i);
+        } while (--i < 0);
+        }
+}
