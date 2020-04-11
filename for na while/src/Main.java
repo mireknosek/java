893318aa92f8cf87f85476c7b0d6127a757f9cc9
mireknosek <mireknosek@gmail.com>
2020-04-11 21:07:53 +0200

    for na while

---
+++ for na while/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+//        int s = 0;
+//        for(int i = 1; i <=10; i++) {
+//            s = s + 1;
+//            System.out.println("i=" + i + ", " + s);
+//        }
+
+        // pętla while
+       int s = 0;
+       int i = 1;
+        while (i <= 10) {
+            s = s + 1;
+       }
+    }
+}
