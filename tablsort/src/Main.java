a610d261891d93bd43b30f20e0c5c8316fac7f45
mireknosek <mireknosek@gmail.com>
2020-04-11 14:35:37 +0200

    cd

---
+++ tablsort/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int tab[] = new int[10];
+        int tabIndex[] = new int[10];
+        int i = 0;
+        Random liczba = new Random();
+        do {
+            tab[i] = liczba.nextInt(10);
+            System.out.println(i + ". " + tab[i]);
+        }
+        while (++i < 10);
+        System.out.println();
+
+        int l;
+        int l1;
+        int temp = tab[0];
+        for (i = 0; i < 9; i++) {
+            l = tab[i];
+            l1 = tab[i + 1];
+            if (l < l1 && l < temp) {
+                temp = l;
+                System.out.print("mniejszy element" + temp);
+                System.out.println();
+            }
+        }
+        System.out.println("najmniejsza wartość: " + temp);
+
+    }
+}
